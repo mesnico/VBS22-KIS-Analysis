1434130aa7adf083334d29fc2c90233fe8233da8
Lucia Vadicamo <lucia.vadicamo@isti.cnr.it>
2022-09-14 19:09:44 +0200

    Added two generation args (max_records and split_user), revised time_recall table generation and rendering function to handle multiple users, fixed a bug in best time calculations (time should be inf if no best rank was found)

--- generate/time_recall_table.py
+++ generate/time_recall_table.py
@@ -10,14 +10,15 @@ class TimeRecallTable(Result):
         self.teams = teams
         self.logs = logs
 
-    def _generate(self):
+    def _generate(self,**kwargs):
         """
         Returns the view of the data interesting for the current analysis, in the form of a Pandas dataframe
         """
-
+        split_user=kwargs.get('split_user', False)
+        max_records=kwargs.get('max_records', 10000)
         dfs = []
         for team in self.teams:
-            df = get_team_values_df(self.data, team, self.logs[team])
+            df = get_team_values_df(self.data, self.logs[team],split_user, max_records)
             dfs.append(df)
 
         total_df = pd.concat(dfs, axis=0)
@@ -27,13 +28,66 @@ class TimeRecallTable(Result):
         """
         Render the dataframe into a table or into a nice graph
         """
-        df = df.melt(var_name="metric", id_vars=["team", "task"], value_name="value")
-        df = df.pivot(index=['team', 'metric'], columns="task", values="value")
-        df=df.fillna(-1)
-        df = df.astype('int32')
-        df[df < 0] = -1
+        df = df.fillna(-1)
+        col=[c for c in df.columns.values.tolist() if c!='team' and c!='task' and c!='user']
+        df[col]= df[col].astype('int32')
+        df[col] = df[col].applymap(lambda x: -1 if x< 0 else x)
         df = df.astype('str')
         df.replace(['-1'], '-', inplace=True)
+
+
+        #renaming tasks
+        rename_fun= lambda x : x.replace('vbs22-kis-t0', 'T_').replace('vbs22-kis-t', 'T_').replace('vbs22-kis-v0', 'V_').replace('vbs22-kis-v', 'V_')
+        df['task']=df['task'].apply(rename_fun)
+
+        #aggregate
+        agg_dic={c: (lambda x: ' / '.join(x)) for c in col}
+        agg_dic['time_correct_submission']="min"
+        df=df.groupby(['team','task'])[col].agg(agg_dic ).reset_index()
+        df.replace('- / -', '-', regex=True, inplace=True)
+
+        df = df.melt(var_name="metric", id_vars=["team", "task"], value_name="value")
+        df['unit'] = df['metric'].apply(lambda x: 'rank' if x.startswith('rank_') else 'time (s)')
+        replace_dic = {
+            'rank_shot_margin_0': 'correct frame',
+            'time_best_shot': 'correct frame',
+            'rank_shot_margin_5': 'frame in GT+2x5s',
+            'time_best_shot_margin5': 'frame in GT+2x5s',
+            'rank_video': 'correct video',
+            'time_best_video': 'correct video',
+            'time_correct_submission': 'correct submission'
+        }
+        df['metric']=df['metric'].map(replace_dic)
+        df = df.pivot(index=['team', 'metric', 'unit'], columns="task", values="value")
+        df=df.fillna('-')
+
+
+        #sorting index desired order
+        level_0=self.teams #order in teh conf file
+        level_1=['correct frame', 'frame in GT+2x5s', 'correct video','correct submission']
+        level_2=['rank','time (s)']
+        df = df.reindex(pd.MultiIndex.from_product([level_0,level_1,level_2]))
+        df.dropna(axis=0, inplace=True)#'correct submission'/rank shluld not be in the index
         print(df)
+        df.to_csv('output/time_recall_table_withMargin5.csv')
+        # sorting index desired order
+        level_0 = self.teams  # order in teh conf file
+        level_1 = ['correct frame', 'correct video', 'correct submission']
+        level_2 = ['rank', 'time (s)']
+        df = df.reindex(pd.MultiIndex.from_product([level_0, level_1, level_2]))
+        df.dropna(axis=0, inplace=True)  # 'correct submission'/rank shluld not be in the index
         df.to_csv('output/time_recall_table.csv')
+        #formatting
+
+
+
+
+
+
+
+
+
+
+
+
 
